2ed2674b8dc65bb5fb8bfb4eafc5951377e4870c
Tate Allen <tateallen@Tates-MacBook-Pro.local>
2016-12-19 19:34:33 -0500

    Revert

--- NLP/vocabulary_handler.py
+++ NLP/vocabulary_handler.py
@@ -18,3 +18,25 @@ class VocabHandler:
 
 print("Loading word embeddings...")
 v = VocabHandler("data/glove.6B.50d.txt")
+
+# import numpy as np
+
+# class VocabHandler:
+# 	def __init__(self, embeddings_path):
+# 		self.vocab_size = 400000
+# 		self.dimensions = 50
+# 		self.vocab_tensor = np.zeros((self.vocab_size, self.dimensions), dtype=np.float32)
+# 		self.vocab_map = {}
+# 		with open(embeddings_path, "r") as emb_file:
+# 			word_index = 0
+# 			for line in emb_file:
+# 				tokens = line.split(" ")
+# 				word = tokens[0]
+# 				values = tokens[1:]
+# 				self.vocab_map[word] = word_index
+# 				for index, val in enumerate(values):
+# 					self.vocab_tensor[word_index][index] = float(val.strip())
+
+# 	def sentence_to_vec(self, sentence):
+# 		tokens = sentence.split(" ")
+# 		return np.array([self.vocab_map[token] for token in tokens], dtype=np.int32)
