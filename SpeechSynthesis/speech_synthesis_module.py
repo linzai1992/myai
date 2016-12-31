ff7867682316899352e34652b91ca9a7a6002ebc
Tate Allen <tateallen@Tates-MacBook-Pro.local>
2016-12-31 17:14:25 -0500

    Logging every 200 steps

--- SpeechSynthesis/speech_synthesis_module.py
+++ SpeechSynthesis/speech_synthesis_module.py
@@ -13,6 +13,7 @@ print("Beginning training")
 epochs = 10
 with tf.Session() as session:
     session.run(tf.global_variables_initializer())
+    step_index = 0
     while True:
         if batcher.epoch_finished():
             input_batches, label_batches = batcher.get_test_batches(50)
@@ -23,5 +24,14 @@ with tf.Session() as session:
             print("Epoch ~ %i" % (test_acc))
             batcher.prepare_epoch()
 
+        if step_index % 200 == 0:
+            input_batches, label_batches = batcher.get_test_batches(50)
+            test_acc = 0
+            for i in range(len(input_batches)):
+                test_acc += model.get_accuracy(session, input_batches[i], label_batches[i])
+            test_acc /= len(input_batches)
+            print("Step %i ~ %f" % (step_index, test_acc))
+
         inputs, labels = batcher.get_training_batch(50)
         model.train_model(session, inputs, labels)
+        step_index += 1
