3ab8833ce568041054466eecaa0715b7db84d68b
Tate Allen <tateallen@Tates-MacBook-Pro.local>
2016-12-31 17:20:55 -0500

    removed birnn

--- SpeechSynthesis/speech_synthesis_model.py
+++ SpeechSynthesis/speech_synthesis_model.py
@@ -9,9 +9,9 @@ class SpeechSynthesisModel:
             self.sequence_length = tf.placeholder(tf.int32, shape=[None])
 
             enc_cell = tf.nn.rnn_cell.LSTMCell(num_units=64)
-            enc_outputs, _ = tf.nn.bidirectional_dynamic_rnn(cell_fw=enc_cell, cell_bw=enc_cell, dtype=tf.float32, sequence_length=self.sequence_length, inputs=self.inputs)
-            enc_output_fw, enc_output_bw = enc_outputs
-            enc_output_final = enc_output_fw * enc_output_bw
+            enc_outputs, _ = tf.nn.dynamic_rnn(cell_fw=enc_cell, cell_bw=enc_cell, dtype=tf.float32, sequence_length=self.sequence_length, inputs=self.inputs)
+            # enc_output_fw, enc_output_bw = enc_outputs
+            enc_output_final = enc_outputs # enc_output_fw * enc_output_bw
 
             dec_cell = tf.nn.rnn_cell.LSTMCell(num_units=total_phons)
             dec_outputs, _ = tf.nn.dynamic_rnn(cell=dec_cell, dtype=tf.float32, inputs=enc_output_final)
