7471dc06459c1eeb6693b78420ef3cfd59717adb
Tate Allen <tateallen@Tates-MacBook-Pro.local>
2016-12-31 13:44:41 -0500

    Added speech synthesis skeleton

---
+++ SpeechSynthesis/speech_synthesis_model.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+class SpeechSynthesisModel:
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def synthesize(self):
+        # beep boop
+        pass
