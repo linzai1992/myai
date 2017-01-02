4c0c8921e25d1f577aa2583cd46b579954731f7e
Tate Allen <tateallen@Tates-MacBook-Pro.local>
2017-01-02 18:51:10 -0500

    Added loading of model

--- SpeechSynthesis/G2P/g2p_model_trainer.py
+++ SpeechSynthesis/G2P/g2p_model_trainer.py
@@ -9,10 +9,18 @@ batcher = G2PDataBatcher("data/cmudict_proc.txt")
 print("Building model...")
 model = G2PModel(batcher.sequence_length, len(batcher.word_character_map), len(batcher.phoneme_map))
 saver = tf.train.Saver()
-print("Beginning training")
+
 with tf.Session() as session:
     session.run(tf.global_variables_initializer())
 
+    print("Loading model...")
+    ckpt = tf.train.get_checkpoint_state("checkpoints/")
+	if ckpt and ckpt.model_checkpoint_path:
+		saver.restore(session, ckpt.model_checkpoint_path)
+	else:
+		print("Failed loading model!")
+
+    print("Beginning training")
     step_index = 0
     epoch_index = 0
     graphemes, phonemes = batcher.get_test_batch()
@@ -36,17 +44,19 @@ with tf.Session() as session:
             loss = model.get_loss(session, graphemes, phonemes)
             print("Step %i ~ loss: %f" % (step_index, loss))
 
-        if step_index == 10000:
+        if step_index == 1000:
             break
 
     while True:
         i = input("Enter word: ").lower().strip()
-        p = model.predict(session, batcher.batch_input_string(i))
-        for i in range(p.shape[0]):
-            word = ""
-            for j in range(batcher.sequence_length):
-                phon = batcher.phoneme_map_inverse[p[i][j]]
-                word += phon + " "
-                if phon == "<END>":
-                    break
-            print(word)
+        words = i.split(" ")
+        for word in words:
+            p = model.predict(session, batcher.batch_input_string(word))
+            for i in range(p.shape[0]):
+                phon_word = ""
+                for j in range(batcher.sequence_length):
+                    phon = batcher.phoneme_map_inverse[p[i][j]]
+                    phon_word += phon + " "
+                    if phon == "<END>":
+                        break
+                print(phon_word)
