bd2340d0b5cc758d890712867acda7428a55706a
Tate Allen <tateallen@Tates-MacBook-Pro.local>
2017-01-03 10:51:35 -0500

    Removed old SS code

--- SpeechSynthesis/speech_synthesis_module.py
+++ SpeechSynthesis/speech_synthesis_module.py
@@ -1,37 +1,5 @@
 from speech_synthesis_model import SpeechSynthesisModel
-from gtp_data_batcher import G2PDataBatcher
-import tensorflow as tf
 
-print("Loading data...")
-batcher = G2PDataBatcher("data/cmudict_proc.txt")
-
-print("Building model...")
-model = SpeechSynthesisModel(batcher.sequence_length, len(batcher.word_character_map), len(batcher.phoneme_map))
-
-print("Beginning training")
-
-epochs = 10
-with tf.Session() as session:
-    session.run(tf.global_variables_initializer())
-    step_index = 0
-    while True:
-        if batcher.epoch_finished():
-            input_batches, label_batches = batcher.get_test_batches(50)
-            test_acc = 0
-            for i in range(len(input_batches)):
-                test_acc += model.get_accuracy(session, input_batches[i], label_batches[i])
-            test_acc /= len(input_batches)
-            print("Epoch ~ %i" % (test_acc))
-            batcher.prepare_epoch()
-
-        if step_index % 200 == 0:
-            input_batches, label_batches = batcher.get_test_batches(50)
-            test_acc = 0
-            for i in range(len(input_batches)):
-                test_acc += model.get_accuracy(session, input_batches[i], label_batches[i])
-            test_acc /= len(input_batches)
-            print("Step %i ~ %f" % (step_index, test_acc))
-
-        inputs, labels = batcher.get_training_batch(50)
-        model.train_model(session, inputs, labels)
-        step_index += 1
+class SpeechSynthesisModule:
+    def __init__(self):
+        pass
