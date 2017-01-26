7739ae15e94a3b7735c68461eebeba1ed7e904b3
Tate Allen <tatea7@yahoo.com>
2017-01-26 18:13:30 -0500

    changes

--- SpeechSynthesisV2/speech_synthesis_trainer.py
+++ SpeechSynthesisV2/speech_synthesis_trainer.py
@@ -27,11 +27,11 @@ with tf.Session() as session:
         if epoch_complete:
             if (current_epoch+1) % 4 == 0:
                 avg_acc = 0.0
-                count = 0
-                for phones_test_batch, spect_test_batch in batcher.get_test_batches():
-                    avg_acc += model.get_loss(session, inputs=phones_test_batch, labels=spect_test_batch, batch_size=phones_test_batch.shape[0])
-                    count += 1
-                avg_acc /= count
-                saver.save(session, os.path.join("checkpoints", "speech_synthesis_model"))
+                # count = 0
+                # for phones_test_batch, spect_test_batch in batcher.get_test_batches():
+                #     avg_acc += model.get_loss(session, inputs=phones_test_batch, labels=spect_test_batch, batch_size=phones_test_batch.shape[0])
+                #     count += 1
+                # avg_acc /= count
+                # saver.save(session, os.path.join("checkpoints", "speech_synthesis_model"))
                 print("Epoch {} | loss: {}".format(current_epoch+1, avg_acc))
             current_epoch += 1
