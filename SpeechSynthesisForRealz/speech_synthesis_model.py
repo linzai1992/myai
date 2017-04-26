b0345aa91b94b3dd24600f8b4742a4247ee0a922
Tate Allen <tatea7@yahoo.com>
2017-04-26 09:55:02 -0700

    Finished CBGH module

--- SpeechSynthesisForRealz/speech_synthesis_model.py
+++ SpeechSynthesisForRealz/speech_synthesis_model.py
@@ -5,12 +5,14 @@ class SpeechSynthesisModel:
     def __init__(self, input_seq_length, char_embedding_size):
         with tf.variable_scope("SpeechSynthesisModelTacotron"):
             self.inputs = tf.placeholder(tf.float32, shape=[None, input_seq_length, char_embedding_size, 1])
-            kek = self.CBHG(self.inputs)
+            encoder_cbhg = self.CBHG(self.inputs)
+
+            print("Encoder CBHG: {}".format(encoder_cbhg.get_shape()))
 
             # ins = tf.placeholder(tf.float32, shape=[None, 128])
             # hi = self.highway(ins)
 
-    def CBHG(self, input_tensor, k=16, in_channels=1, out_channels=128):
+    def CBHG(self, input_tensor, k=16, in_channels=1, out_channels=128, gru_units=128):
         with tf.variable_scope("CBHG"):
             tensor_width = int(input_tensor.get_shape()[-2])
             conv_bank_outputs = list()
@@ -58,32 +60,41 @@ class SpeechSynthesisModel:
             concatenated_projections_act = tf.concat(projections_act, axis=-2, name="concatenated_projections_act")
             padded_projections_act = tf.pad(concatenated_projections_act, paddings=[[0,0], [1,1], [0,0], [0,0]], mode="CONSTANT")
             embedding_size = int(input_tensor.get_shape()[-2])
+            projections = list();
             for _ in range(3):
                 w = tf.Variable(tf.truncated_normal([3, 3, out_channels, embedding_size], stddev=0.1))
                 b = tf.Variable(tf.constant(0.1, shape=[out_channels]))
                 conv = tf.nn.conv2d(padded_projections_act, filter=w, strides=[1,1,1,1], padding="VALID")
                 conv_residual = tf.transpose(conv, perm=[0,1,3,2]) + input_tensor
-                print(conv_residual.get_shape())
+                flattened = tf.reshape(conv_residual, shape=[-1, out_channels * embedding_size])
+                projections.append(flattened)
+                print(flattened.get_shape())
 
-            # pooled_banks = tf.nn.max_pool(concatenated_banks, ksize=[1,2,tensor_width,1], strides=[1,1,1,1], padding="SAME", name="pooled_banks")
+            print("Highway Input =========================")
+            concatenated_projections = tf.concat(projections, axis=-1)
+            print(concatenated_projections.get_shape())
 
-            # projections = list()
-            # for i in range(3):
-            #     w = tf.Variable(tf.truncated_normal([3, tensor_width, out_channels * k, out_channels], stddev=0.1))
-            #     b = tf.Variable(tf.constant(0.1, shape=[out_channels]))
-            #     conv = tf.nn.conv2d(pooled_banks, filter=w, strides=[1,1,1,1], padding="SAME")
-            #     act = tf.nn.elu(conv + b, name="1d_conv_projection_{}".format(i+1))
-            #     projections.append(act)
-            #
-            # print(input_tensor.get_shape())
-            # print("Final =========================")
-            # for p in projections:
-            #     print(p.get_shape())
+            highway_layers = self.highway(concatenated_projections, layers=4, hidden_size=128)
+            print("Highway output ========================")
+            print(highway_layers.get_shape())
+
+            gru_input = tf.expand_dims(highway_layers, axis=-1)
+            rnn_cell_forward = tf.contrib.rnn.GRUCell(gru_units)
+            rnn_cell_backward = tf.contrib.rnn.GRUCell(gru_units)
+            brnn_outputs, brnn_states = tf.nn.bidirectional_dynamic_rnn(cell_fw=rnn_cell_forward, cell_bw=rnn_cell_backward, inputs=gru_input, dtype=tf.float32)
+            print("GRU output ============================")
+            print(brnn_outputs[0].get_shape())
+
+            final_rnn_output = brnn_outputs[0] + brnn_outputs[1]
+            return final_rnn_output
 
     def highway(self, input_tensor, layers=4, hidden_size=128):
         with tf.variable_scope("Highway"):
-            last_input = input_tensor
-            for _ in range(layers):
+            w_l1 = tf.Variable(tf.truncated_normal([int(input_tensor.get_shape()[-1]), hidden_size], stddev=0.1))
+            b_l1 = tf.Variable(tf.constant(0.1, shape=[hidden_size]))
+            h_x_l1 = tf.nn.elu(tf.nn.xw_plus_b(input_tensor, w_l1, b_l1))
+            last_input = h_x_l1
+            for _ in range(layers - 1):
                 size = int(last_input.get_shape()[-1])
 
                 w_h = tf.Variable(tf.truncated_normal([size, hidden_size], stddev=0.1))
@@ -99,4 +110,4 @@ class SpeechSynthesisModel:
                 last_input = y
             return last_input
 
-m = SpeechSynthesisModel(100, 50)
+m = SpeechSynthesisModel(50, 256)
